36573bec32666b2b5fbd13d50a551d52a163d14c
jiahong <qu_jia_hong@163.com>
2017-11-28 17:27:48 +0800

    更新了cocoapods，导入了Alamofire

--- 31-设置导航栏/HomeViewController.swift
+++ 31-设置导航栏/HomeViewController.swift
@@ -7,6 +7,8 @@
 //
 
 import UIKit
+import Alamofire
+
 // MARK: - 类的声明
 class HomeViewController: UIViewController {
 
---
+++ Pods/Target Support Files/Pods-31-设置导航栏/Pods-31-设置导航栏-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_31______ : NSObject
+@end
+@implementation PodsDummy_Pods_31______
+@end
